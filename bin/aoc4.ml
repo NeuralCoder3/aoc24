567a61b4fea909ab482988db9884eeeccaa9463c
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2024-12-04 13:26:24 +0100

    simplify using slices

--- bin/aoc4.ml
+++ bin/aoc4.ml
@@ -11,52 +11,21 @@ let data =
 let width = Array.length data.(0)
 let height = Array.length data
 
+let slice arr (ox,oy) len (sx,sy) =
+  List.init len (fun i -> arr.(oy + i*sy).(ox + i*sx))
+
 let count x y =
-  (* check if horizontally XMAS *)
-  (if 
-    x >= 3 && 
-    ((data.(y).(x) = 'S' 
-    && data.(y).(x-1) = 'A'
-    && data.(y).(x-2) = 'M'
-    && data.(y).(x-3) = 'X') ||
-    (data.(y).(x) = 'X'
-    && data.(y).(x-1) = 'M'
-    && data.(y).(x-2) = 'A'
-    && data.(y).(x-3) = 'S'))
-  then 1 else 0)+
-  (* check if vertically XMAS *)
-  (if y >= 3 && 
-    ((data.(y).(x) = 'S' 
-    && data.(y-1).(x) = 'A'
-    && data.(y-2).(x) = 'M'
-    && data.(y-3).(x) = 'X') ||
-    (data.(y).(x) = 'X'
-    && data.(y-1).(x) = 'M'
-    && data.(y-2).(x) = 'A'
-    && data.(y-3).(x) = 'S'))
-  then 1 else 0)+
-  (* check if diagonally XMAS *)
-  (if x >= 3 && y >= 3 && 
-    ((data.(y).(x) = 'S' 
-    && data.(y-1).(x-1) = 'A'
-    && data.(y-2).(x-2) = 'M'
-    && data.(y-3).(x-3) = 'X') ||
-    (data.(y).(x) = 'X'
-    && data.(y-1).(x-1) = 'M'
-    && data.(y-2).(x-2) = 'A'
-    && data.(y-3).(x-3) = 'S'))
-  then 1 else 0)+
-  (if x >= 3 && y < height - 3 && 
-    ((data.(y).(x) = 'S' 
-    && data.(y+1).(x-1) = 'A'
-    && data.(y+2).(x-2) = 'M'
-    && data.(y+3).(x-3) = 'X') ||
-    (data.(y).(x) = 'X'
-    && data.(y+1).(x-1) = 'M'
-    && data.(y+2).(x-2) = 'A'
-    && data.(y+3).(x-3) = 'S'))
-  then 1 else 0)+
-  0
+  let valid_slice xs =
+    xs = ['X';'M';'A';'S'] || xs = ['S';'A';'M';'X']
+  in
+  (* horizontal *)
+  (if x < width - 3 && valid_slice (slice data (x,y) 4 (1,0)) then 1 else 0) +
+  (* vertical *)
+  (if y < height - 3 && valid_slice (slice data (x,y) 4 (0,1)) then 1 else 0) +
+  (* diagonal *)
+  (if x < width - 3 && y < height - 3 && valid_slice (slice data (x,y) 4 (1,1)) then 1 else 0) +
+  (if x >= 3 && y < height - 3 && valid_slice (slice data (x,y) 4 (-1,1)) then 1 else 0)
+
 
 let full_count f =
   let count = 
@@ -70,37 +39,11 @@ let () = full_count count |> Printf.printf "Part1: %d\n%!"
 
 let count2 x y =
   (* MAS cross with MAS *)
+  let valid_slice xs = xs = ['M';'A';'S'] || xs = ['S';'A';'M'] in
   if 
     x < width - 2 && y < height - 2 && 
-    (* left one from top to bottom, right one from top to bottom *)
-    ((data.(y).(x) = 'M'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x+2) = 'S'
-    && data.(y).(x+2) = 'M'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x) = 'S') ||
-    (* left flipped *)
-    (data.(y).(x) = 'S'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x+2) = 'M'
-    && data.(y).(x+2) = 'M'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x) = 'S') ||
-    (* right flipped *)
-    (data.(y).(x) = 'M'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x+2) = 'S'
-    && data.(y).(x+2) = 'S'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x) = 'M') ||
-    (* both flipped *)
-    (data.(y).(x) = 'S'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x+2) = 'M'
-    && data.(y).(x+2) = 'S'
-    && data.(y+1).(x+1) = 'A'
-    && data.(y+2).(x) = 'M')
-    )
+    valid_slice (slice data (x,y) 3 (1,1)) &&
+    valid_slice (slice data (x+2,y) 3 (-1,1)) 
   then 1 else 0
 
 let () = full_count count2 |> Printf.printf "Part2: %d\n%!"
